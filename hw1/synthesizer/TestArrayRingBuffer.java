04d0a5e89a6424057af3d1c57eb422c8a484139c
dada0804 <62565567+dada0804@users.noreply.github.com>
2021-11-13 01:13:32 +0800

    homework 1

--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -9,7 +9,23 @@ import static org.junit.Assert.*;
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer arb = new ArrayRingBuffer(3);
+        arb.enqueue(1);
+        arb.enqueue(2);
+        arb.enqueue(3);
+        assertTrue(arb.isFull());
+        arb.dequeue();
+        arb.dequeue();
+        arb.dequeue();
+        assertTrue(arb.isEmpty());
+        arb.enqueue(3);
+        int expected = 1;
+        assertEquals(expected, arb.fillCount);
+        int expected2 = 3;
+        assertEquals(expected2, arb.peek());
+
+
+
     }
 
     /** Calls tests for ArrayRingBuffer. */
