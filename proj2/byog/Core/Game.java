429b5750911deca37a44e0561e0b1d355d38fa7d
dada0804 <62565567+dada0804@users.noreply.github.com>
2021-11-17 20:52:48 +0800

    project phase 1

--- proj2/byog/Core/Game.java
+++ proj2/byog/Core/Game.java
@@ -2,12 +2,26 @@ package byog.Core;
 
 import byog.TileEngine.TERenderer;
 import byog.TileEngine.TETile;
+import byog.TileEngine.Tileset;
+import edu.princeton.cs.introcs.StdDraw;
+
+import java.util.Random;
+
 
 public class Game {
     TERenderer ter = new TERenderer();
     /* Feel free to change the width and height. */
     public static final int WIDTH = 80;
     public static final int HEIGHT = 30;
+
+    //why?
+    private static final int MAX_ROOMLENGTH = 8;
+    private static final int MIN_ROOMLENGTH = 3;
+
+    private static Random RANDOM;
+    private static Rooms[] rooms;
+
+    private static int roomNum;
 
     /**
      * Method used for playing a fresh game. The game should start from the main menu.
@@ -32,7 +46,226 @@ public class Game {
         // and return a 2D tile representation of the world that would have been
         // drawn if the same inputs had been given to playWithKeyboard().
 
-        TETile[][] finalWorldFrame = null;
+
+        Long seed = -1L;
+        if(input.toLowerCase().contains("n") && input.toLowerCase().contains("s")){
+            int start = input.toLowerCase().indexOf("n") + 1;
+            int end = input.toLowerCase().indexOf("s");
+            try {
+                seed = Long.parseLong(input.substring(start, end));
+            }catch(Exception e){
+                throw new RuntimeException("Seed has to be an integer. You entered: \"" + input.substring(start, end) + "\"");
+            }
+        }
+
+        setSeed(seed);
+        ter.initialize(WIDTH, HEIGHT);
+        TETile[][] randomWorld = new TETile[WIDTH][HEIGHT];
+        for (int x = 0; x < WIDTH; x += 1) {
+            for (int y = 0; y < HEIGHT; y += 1) {
+                randomWorld[x][y] = Tileset.NOTHING;
+            }
+        }
+
+        initializeRandomRooms(100);
+        setRooms(randomWorld);
+        ter.renderFrame(randomWorld);
+        TETile[][] finalWorldFrame = randomWorld;
         return finalWorldFrame;
     }
+
+    public static void setSeed(Long seed){
+        RANDOM = new Random(seed);
+    }
+
+    private static boolean collidedWithPreviousRooms(Rooms[] rooms, int i){
+        for(int j = 0; j <= i; j++){
+            if(rooms[i+1].isCollided(rooms[j])){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void branchTop(Rooms[] rooms, int i) {
+        if (HEIGHT - (rooms[i].y + rooms[i].roomHeight) < MIN_ROOMLENGTH) {
+            return;
+        }
+
+        //x应该是width上任意一点
+        //如果该room的width和最小长度一样，那么x（也就是上面房间的x）就等于这个room的x，否则等于随机数（要把范围传进去，0-该数值-1）
+        int x = (rooms[i].roomWidth == MIN_ROOMLENGTH) ?
+                rooms[i].x : RANDOM.nextInt(rooms[i].roomWidth - MIN_ROOMLENGTH) + rooms[i].x;
+        //y坐标就是在此基础上加上height
+        int y = rooms[i].y + rooms[i].roomHeight;
+        //新房子的宽度要么是在3-8之间，要么是整个房间宽度-x坐标轴，选取这两个小的（以防超出大房间范围）
+        int width = Math.min(RANDOM.nextInt(MAX_ROOMLENGTH - MIN_ROOMLENGTH) + MIN_ROOMLENGTH, WIDTH - x);
+        int height = Math.min(RANDOM.nextInt(MAX_ROOMLENGTH - MIN_ROOMLENGTH) + MIN_ROOMLENGTH, HEIGHT - y);
+
+        rooms[i + 1] = new Rooms(x, y, width, height);
+        if (collidedWithPreviousRooms(rooms, i)) {
+            rooms[i + 1] = null;
+        } else {
+            //create doors
+            int leftBound = rooms[i + 1].x + 1;
+            //based on which one is left or right
+            int rightBound = Math.min(rooms[i].x + rooms[i].roomWidth - 2, rooms[i + 1].x + rooms[i + 1].roomWidth - 2);
+
+            int doorX0 = (rightBound - leftBound == 0) ? rightBound : RANDOM.nextInt(rightBound - leftBound) + leftBound;
+            int doorY0 = y - 1;
+            int doorX1 = doorX0;
+            int doorY1 = y;
+
+            rooms[i].createDoor(1, doorX0, doorY0);
+            rooms[i + 1].createDoor(0, doorX1, doorY1);
+        }
+    }
+
+    public static  void branchBot(Rooms[] rooms, int i){
+        if (rooms[i].y < MIN_ROOMLENGTH){
+            return;
+        }
+        int x = (rooms[i].roomWidth == MIN_ROOMLENGTH) ?
+                rooms[i].x : RANDOM.nextInt(rooms[i].roomWidth - MIN_ROOMLENGTH) + rooms[i].x;
+        int width = Math.min(RANDOM.nextInt(MAX_ROOMLENGTH - MIN_ROOMLENGTH) + MIN_ROOMLENGTH, WIDTH - x);
+        int height = Math.min(RANDOM.nextInt(MAX_ROOMLENGTH - MIN_ROOMLENGTH) + MIN_ROOMLENGTH, rooms[i].y);
+        int y = rooms[i].y - height;
+
+        rooms[i + 1] = new Rooms(x, y, width, height);
+        if(collidedWithPreviousRooms(rooms, i)){
+            rooms[i + 1] = null;
+        }else{
+            //create doors when branching top.
+            int leftBound = rooms[i + 1].x + 1;
+            int rightBound = Math.min(rooms[i].x + rooms[i].roomWidth - 2, rooms[i + 1].x + rooms[i + 1].roomWidth - 2);
+            //x + rW - 1 is the x pos of the room's right most block, need to use x + rW - 2 because the door can't be at the corner.
+
+            int doorX0 = (rightBound - leftBound == 0) ? rightBound : RANDOM.nextInt(rightBound - leftBound) + leftBound;
+            int doorY0 = rooms[i].y;
+            int doorX1 = doorX0;
+            int doorY1 = rooms[i].y - 1;
+
+            rooms[i].createDoor(1, doorX0, doorY0);
+            rooms[i + 1].createDoor(0, doorX1, doorY1);
+        }
+    }
+
+    public static void branchLeft(Rooms[] rooms, int i){
+        if (rooms[i].x < MIN_ROOMLENGTH){
+            return;
+        }
+        int y = (rooms[i].roomHeight == MIN_ROOMLENGTH) ?
+                rooms[i].y : RANDOM.nextInt(rooms[i].roomHeight - MIN_ROOMLENGTH) + rooms[i].y;
+        int height = (Math.min(RANDOM.nextInt(MAX_ROOMLENGTH - MIN_ROOMLENGTH) + MIN_ROOMLENGTH, HEIGHT - y));
+        int width = Math.min(RANDOM.nextInt(MAX_ROOMLENGTH - MIN_ROOMLENGTH) + MIN_ROOMLENGTH, rooms[i].x);
+        int x = rooms[i].x - width;
+
+        rooms[i + 1] = new Rooms(x, y, width, height);
+        if(collidedWithPreviousRooms(rooms, i)){
+            rooms[i + 1] = null;
+        }else{
+            //create doors for two rooms when branching left.
+            int botBound = rooms[i + 1].y + 1;
+            int topBound = Math.min(rooms[i].y + rooms[i].roomHeight - 2, rooms[i + 1].y + rooms[i + 1].roomHeight - 2);
+
+            int doorX0 = rooms[i].x;
+            int doorY0 = (topBound - botBound == 0) ? topBound : RANDOM.nextInt(topBound - botBound) + botBound;
+            int doorX1 = rooms[i].x - 1;
+            int doorY1 = doorY0;
+
+            rooms[i].createDoor(1, doorX0, doorY0);
+            rooms[i + 1].createDoor(0, doorX1, doorY1);
+
+        }
+    }
+    public static void branchRight(Rooms[] rooms, int i){
+        if (WIDTH - (rooms[i].x + rooms[i].roomWidth) < MIN_ROOMLENGTH){
+            return;
+        }
+        int y = (rooms[i].roomHeight == MIN_ROOMLENGTH) ?
+                rooms[i].y : RANDOM.nextInt(rooms[i].roomHeight - MIN_ROOMLENGTH) + rooms[i].y;
+        int x = rooms[i].x + rooms[i].roomWidth;
+        int height = (Math.min(RANDOM.nextInt(MAX_ROOMLENGTH - MIN_ROOMLENGTH) + MIN_ROOMLENGTH, HEIGHT - y));
+        int width = Math.min(RANDOM.nextInt(MAX_ROOMLENGTH - MIN_ROOMLENGTH) + MIN_ROOMLENGTH, WIDTH - x);
+
+        rooms[i + 1] = new Rooms(x, y, width, height);
+        if(collidedWithPreviousRooms(rooms, i)){
+            rooms[i + 1] = null;
+        }else{
+            //create doors for two rooms when branching right.
+            int botBound = rooms[i + 1].y + 1;
+            int topBound = Math.min(rooms[i].y + rooms[i].roomHeight - 2, rooms[i + 1].y + rooms[i + 1].roomHeight - 2);
+
+            int doorX0 = x - 1;
+            int doorY0 = (topBound - botBound == 0) ? topBound : RANDOM.nextInt(topBound - botBound) + botBound;
+            int doorX1 = x;
+            int doorY1 = doorY0;
+
+            rooms[i].createDoor(1, doorX0, doorY0);
+            rooms[i + 1].createDoor(0, doorX1, doorY1);
+        }
+    }
+
+    public static void branchRoom(Rooms[] rooms, int i){
+        int trial = 0;
+        while (rooms[i + 1] == null){
+            switch(RANDOM.nextInt(4)){
+                case 0:
+                    branchTop(rooms, i);
+                    trial++;
+                    break;
+                case 1:
+                    branchBot(rooms, i);
+                    trial++;
+                    break;
+                case 2:
+                    branchLeft(rooms, i);
+                    trial++;
+                    break;
+                case 3:
+                    branchRight(rooms, i);
+                    trial++;
+                    break;
+                default:
+                    break;
+            }
+            if(trial >= 50){
+                roomNum = i;
+                System.out.println("Failed to create room[" + Integer.toString(i + 1) + "], total Room number: " + Integer.toString(roomNum));
+
+                break;
+            }
+        }
+    }
+
+    public static void initializeRandomRooms(int n) {
+        rooms = new Rooms[n];
+        int x = RANDOM.nextInt(WIDTH - MIN_ROOMLENGTH);
+        int y = RANDOM.nextInt(HEIGHT - MIN_ROOMLENGTH);
+        int roomWidth = Math.min(RANDOM.nextInt(MAX_ROOMLENGTH - MIN_ROOMLENGTH) + MIN_ROOMLENGTH, WIDTH - x);
+        int roomHeight = Math.min(RANDOM.nextInt(MAX_ROOMLENGTH - MIN_ROOMLENGTH) + MIN_ROOMLENGTH, HEIGHT - y);
+        rooms[0] = new Rooms(x, y, roomWidth, roomHeight);
+
+        //branch
+        for (int i = 0; i < n -1; i++){
+            branchRoom(rooms, i);
+            if (rooms[i+1] == null){
+                break;
+            }
+        }
+
+
+    }
+
+    public static void setRooms(TETile[][] tiles){
+        for(int i = 0; i < rooms.length; i++){
+            if(rooms[i] == null){
+                break;
+            }
+            rooms[i].draw(tiles);
+        }
+    }
+
+
+
 }
--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -12,4 +12,121 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+    private static final int WIDTH = 50;
+    private static final int HEIGHT = 50;
+
+    private static final long SEED = 2873123;
+    private static final Random RANDOM = new Random(SEED);
+
+
+    public static void drawWorld(TETile[][] world){
+        for (int x = 0; x < WIDTH; x += 1) {
+            for (int y = 0; y < HEIGHT; y += 1) {
+                world[x][y] = Tileset.NOTHING;
+            }
+        }
+    }
+
+    private static class Position{
+        int xpos;
+        int ypos;
+        private Position(int x, int y){
+            xpos = x;
+            ypos = y;
+        }
+    }
+
+    public static Position addoneHexagon(TETile[][] world, int s, Position p, TETile t){
+        //bottom up
+        for (int y = 0; y < s; y += 1){
+            for (int x = p.xpos - y; x< p.xpos+s-1+1*y+1; x+=1){
+                world[x][p.ypos+y] = t;
+            }
+        }
+
+        //change position to middle
+        p.xpos = p.xpos - ( s - 1);
+        p.ypos = p.ypos + s;
+
+        //top down
+        for (int y = 0; y < s; y += 1){
+            for (int x = p.xpos+y; x < p.xpos+s-1+2*(s-1)-y+1; x+=1){
+                world[x][y+ p.ypos] = t;
+            }
+        }
+        //next's left bottom
+        p.xpos = p.xpos + (s-1);
+        p.ypos = p.ypos + s;
+        // test
+        // world[p.xpos][p.ypos]=Tileset.FLOWER;
+        return p;
+
+    }
+
+    public static void addHexagon(TETile[][] world, Position p, int s){
+        // initial position for vertical set
+        p.xpos = s;
+        p.ypos = 2*s + 1;
+        for (int x = 0; x < 3; x++){
+            p = addoneHexagon(world, s, p, randomTile() );
+        }
+        p.xpos = s+s+s-1;
+        p.ypos = s + 1;
+        for (int x = 0; x < 4; x++){
+            p = addoneHexagon(world, s, p, randomTile() );
+        }
+
+        p.xpos = s+2*(s+s-1);
+        p.ypos = 1;
+        for (int x = 0; x < 5; x++){
+            p = addoneHexagon(world, s, p, randomTile() );
+        }
+
+        p.xpos = s+3*(s+s-1);
+        p.ypos = s + 1;
+        for (int x = 0; x < 4; x++){
+            p = addoneHexagon(world, s, p, randomTile() );
+        }
+
+        p.xpos = s+4*(s+s-1);
+        p.ypos = 2*s + 1;
+        for (int x = 0; x < 3; x++){
+            p = addoneHexagon(world, s, p, randomTile() );
+        }
+
+
+    }
+
+    private static TETile randomTile() {
+        int tileNum = RANDOM.nextInt(5);
+        switch (tileNum) {
+            case 0:
+                return Tileset.SAND;
+            case 1:
+                return Tileset.FLOWER;
+            case 2:
+                return Tileset.GRASS;
+            case 3:
+                return Tileset.MOUNTAIN;
+            case 4:
+                return Tileset.TREE;
+            default:
+                return Tileset.NOTHING;
+        }
+    }
+
+    public static void main(String[] args) {
+        // initialize the tile rendering engine with a window of size WIDTH x HEIGHT
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+
+        TETile[][] world = new TETile[WIDTH][HEIGHT];
+        Position position = new Position(4,0);
+        drawWorld(world);
+//        addoneHexagon(world, 5, position);
+        addHexagon(world, position, 3);
+
+        ter.renderFrame(world);
+    }
 }
+
