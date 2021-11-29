4be6d4df4843ee28b2497db01a3324978d6d1953
dada0804 <62565567+dada0804@users.noreply.github.com>
2021-11-29 20:57:23 +0800

    lab 11 - complete optional

--- lab11/lab11/graphs/MazeAStarPath.java
+++ lab11/lab11/graphs/MazeAStarPath.java
@@ -1,5 +1,8 @@
 package lab11.graphs;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  *  @author Josh Hug
  */
@@ -8,6 +11,16 @@ public class MazeAStarPath extends MazeExplorer {
     private int t;
     private boolean targetFound = false;
     private Maze maze;
+    private PriorityQueue<Integer> fringe = new PriorityQueue(new Comparator() {
+        @Override
+        public int compare(Object o1, Object o2) {
+            return distTo[(int) o1] + h((int)o1) - distTo[(int)o2] - h((int)o2);
+
+//            return distTo[(int) o1] + h((int)o1) - distTo[(int)o2] - h((int)o2);
+        }
+    }){
+
+    };
 
     public MazeAStarPath(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
@@ -20,7 +33,8 @@ public class MazeAStarPath extends MazeExplorer {
 
     /** Estimate of the distance from v to the target. */
     private int h(int v) {
-        return -1;
+        return Math.abs(maze.toX(v) - maze.toX(t)) + Math.abs(maze.toY(v) - maze.toY(t));
+
     }
 
     /** Finds vertex estimated to be closest to target. */
@@ -31,7 +45,27 @@ public class MazeAStarPath extends MazeExplorer {
 
     /** Performs an A star search from vertex s. */
     private void astar(int s) {
-        // TODO
+
+        fringe.add(s);
+
+        while (!fringe.isEmpty()){
+            marked[s] = true;
+            if (s == t){
+                targetFound = true;
+                return;
+            }
+            for (int w : maze.adj(s)){
+                if (! marked[w]){
+                    edgeTo[w] = s;
+                    announce();
+                    distTo[w] = distTo[s] + 1;
+                    fringe.add(w);
+                }
+            }
+            s = fringe.remove();
+
+        }
+
     }
 
     @Override
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,7 @@
 package lab11.graphs;
 
+import java.util.Stack;
+
 /**
  *  @author Josh Hug
  */
@@ -9,16 +11,42 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private boolean hasCycle = false;
+    private Maze maze;
+//    private Stack x = new Stack();
 
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
+        distTo[s] = 0;
+        edgeTo[s] = s;
+
     }
 
     @Override
     public void solve() {
+        dfs(maze, s);
         // TODO: Your code here!
     }
 
     // Helper methods go here
+    public void dfs(Maze m, int v){
+        marked[v] = true;
+        announce();
+        for (int w : m.adj(v)){
+            if (!marked[w]){
+                edgeTo[w] = v;
+                announce();
+                distTo[w] = distTo[v] + 1;
+                dfs(m,w);
+                }
+
+            if(marked[w] && ! (distTo[v] - distTo[w] == 1)){
+                hasCycle = true;
+                return;
+            }
+        }
+    }
 }
 
