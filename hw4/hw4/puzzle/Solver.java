101a70b3477080558d25272edab7d71277386279
dada0804 <62565567+dada0804@users.noreply.github.com>
2021-11-30 18:30:29 +0800

    test solver

--- hw4/hw4/puzzle/Solver.java
+++ hw4/hw4/puzzle/Solver.java
@@ -8,10 +8,10 @@ public class Solver {
     private int moves;
     private ArrayList<WorldState> solution= new ArrayList<>();
     private searchNode search;
-    private ArrayList<WorldState> allAdded= new ArrayList<>();
+    private ArrayList<WorldState> allAdded= new ArrayList<>(); //测试有多少enqueue了
 
 
-
+// 终于写出下面这个表示很满意
     private class searchNode{
         private WorldState cur;
         private int move;
@@ -25,41 +25,37 @@ public class Solver {
 
 
 
-    public Solver(WorldState initial){
+    public Solver(WorldState initial) {
         MinPQ<searchNode> pq = new MinPQ<searchNode>(new Comparator<searchNode>() {
             @Override
             public int compare(searchNode o1, searchNode o2) {
-                return (o1.move+o1.cur.estimatedDistanceToGoal())-(o2.move+o2.cur.estimatedDistanceToGoal());
+                return (o1.move + o1.cur.estimatedDistanceToGoal()) - (o2.move + o2.cur.estimatedDistanceToGoal());
             }
 
         });
-        searchNode first = new searchNode (initial, 0, null);
+        searchNode first = new searchNode(initial, 0, null);
         pq.insert(first);
         allAdded.add(first.cur);
-        while (!pq.isEmpty()){
+        while (!pq.isEmpty()) {
             searchNode removed = pq.delMin();
-            if (removed.cur.isGoal()){
+            if (removed.cur.isGoal()) {
                 moves = removed.move;
                 search = removed;
                 return;
             }
             for (WorldState x : removed.cur.neighbors()) {
-                if (removed.prev == null) {
-                    searchNode next = new searchNode(x, removed.move + 1, removed);
-                    pq.insert(next);
-                    allAdded.add(next.cur);
+// 啊啊啊气死了，搞了一下午原来是因为equals搞错了；一定要记住只要基本类型才用==；reference这种都用equals
+                if (removed.prev != null && x.equals(removed.prev.cur)) {
+                    continue;
                 }
-                if (removed.prev != null && x != removed.prev.cur) {
+                searchNode next = new searchNode(x, removed.move + 1, removed);
 
-                    searchNode next = new searchNode(x, removed.move + 1, removed);
-                    pq.insert(next);
-                    allAdded.add(next.cur);
+                pq.insert(next);
+                allAdded.add(next.cur);
 //                这样计算出来应该是不对的；因为allAdded只是单纯的放进去，而pq则是排序，再remove；一共放了50个，但是到25个就已经有答案了
-                }
-            }
-
             }
         }
+    }
 
 
     public int moves(){
@@ -75,7 +71,7 @@ public class Solver {
     }
 
     public Iterable<WorldState> solution(){
-        while(search.prev != null){
+        while(search!= null){
             solution.add(search.cur);
             search = search.prev;
         }
